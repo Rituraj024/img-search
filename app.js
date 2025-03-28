1d200f85f48ba6b6109a3bee864972572a0a6113
Rituraj <rituraj2924@gmail.com>
2025-03-28 16:14:56 +0530

    final

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const acesskey = "BCU5nowjJ12JSoc-3AfukEklcQNKcrP-bL58Nq4xLSE"
 const form1 = document.querySelector("form");
 const inp = document.getElementById("search-inp");
 const btn = document.getElementsByClassName("search-result");
-const btn2 = document.getElementsById("show-more")
+const btn2 = document.getElementById("show-more");
 
 let inpData = "";
 let page = 1;
@@ -13,7 +13,7 @@ async function getImages() {
   const res = await fetch(url);
   const data = await res.json();
   console.log(data);
-  const result = data.result;
+  //const result = data.result;
 
   if(page===1){
     btn.innerHTML = "";
@@ -21,5 +21,19 @@ async function getImages() {
   result.map((result)=>{
     const imgWrapper = document.createElement("div");
     imgWrapper.classList.add("search-res");
+    const image = document.createElement("img");
+    image.src = result.urls.regular;
+    image.alt = data.alt_description; 
+    imgWrapper.appendChild(image);
+    btn.appendChild(imgWrapper);
   })
+  page++;
+  if(page>1){
+    btn2.style.display = "block";
+  }
 }
+form1.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  getImages();
+  inp.value = "";
+})
